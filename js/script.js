59560dbcefc0de917c54e2e8a98137f27bee5b8f
Lenoricus <helenoricus@gmail.com>
2023-09-07 01:48:56 +0700

    ready draft version

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 function showElement(button) {
+    let infoElements = document.getElementsByClassName("navigation-info");
+
+    for (let i = 0; i < infoElements.length; i++) {
+        infoElements[i].classList.remove("show");
+    }
+
     let infoElement = button.nextElementSibling;
-    infoElement.classList.toggle("show");
+    infoElement.classList.add("show");
 }
+
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
         let dropdowns = document.getElementsByClassName("navigation-info");
-        let i;
-        for (i = 0; i < dropdowns.length; i++) {
+        for (let i = 0; i < dropdowns.length; i++) {
             let openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show') ) {
+            if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
             }
         }
